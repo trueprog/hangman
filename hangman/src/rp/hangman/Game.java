182546ff218940b5f8589614b9b8381e5ee7aac5
andwh <andwh@DESKTOP-DP85DUB.fritz.box>
2017-01-14 15:55:50 +0100

    Create a frame and show it.

---
+++ hangman/src/rp/hangman/Game.java
@@ -0,0 +1,49 @@
+package rp.hangman;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+
+	public static void main(String[] args) {
+        launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Hangman");
+		StackPane root = new StackPane();
+		Image bgImage = addBackground(root);
+		
+		
+		Button btn = new Button();
+		btn.setText("Say 'Hello World'");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("Hello World!");
+			}
+		});
+
+		root.getChildren().add(btn);
+		primaryStage.setScene(new Scene(root, bgImage.getWidth(), bgImage.getHeight()));
+		primaryStage.show();
+	}
+
+	private Image addBackground(StackPane root) {
+		Image bgImage = new Image("./jungle.jpg");
+		ImageView bgView = new ImageView();
+		bgView.setImage(bgImage);
+		root.getChildren().add(bgView);
+		return bgImage;
+	}
+
+}
