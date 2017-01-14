baf16536a15571b903434ceca42e5ea92d56a7be
andwh <andwh@DESKTOP-DP85DUB.fritz.box>
2017-01-14 16:43:11 +0100

    Some initial UI elements

--- hangman/src/rp/hangman/Game.java
+++ hangman/src/rp/hangman/Game.java
@@ -3,14 +3,28 @@ package rp.hangman;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Game extends Application {
+
+	private Image bgImage;
+	private Label question;
+	private TextField answer;
+	private Canvas canvas;
 
 	public static void main(String[] args) {
         launch(args);
@@ -20,30 +34,34 @@ public class Game extends Application {
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Hangman");
 		StackPane root = new StackPane();
-		Image bgImage = addBackground(root);
-		
-		
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello World!");
-			}
-		});
-
-		root.getChildren().add(btn);
+		addBackground(root);
+	    addCanvas(root);
+	    addQA(root);
 		primaryStage.setScene(new Scene(root, bgImage.getWidth(), bgImage.getHeight()));
 		primaryStage.show();
 	}
 
-	private Image addBackground(StackPane root) {
-		Image bgImage = new Image("./jungle.jpg");
+	private void addCanvas(StackPane root) {
+		canvas = new Canvas(bgImage.getWidth(), bgImage.getHeight());
+	    root.getChildren().add(canvas);
+	}
+
+	private void addQA(StackPane root) {
+		question = new Label("A question");
+	    question.setFont(Font.font("serif", FontWeight.BOLD, 20));
+	    answer = new TextField();
+	    VBox vbox = new VBox();
+	    vbox.setPadding(new Insets(10));
+	    vbox.setSpacing(8);
+	    vbox.getChildren().addAll(question, answer);
+		root.getChildren().add(vbox);
+	}
+
+	private void addBackground(StackPane root) {
+		bgImage = new Image("./jungle.jpg");
 		ImageView bgView = new ImageView();
 		bgView.setImage(bgImage);
 		root.getChildren().add(bgView);
-		return bgImage;
 	}
 
 }
