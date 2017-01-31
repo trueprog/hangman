6ae162c4ada79029e4165ee2f692a846bfc3648a
trueprog <trueprogandy@gmail.com>
2017-01-31 17:11:36 +0100

    Added winning and losing.

--- src/rp/hangman/Game.java
+++ src/rp/hangman/Game.java
@@ -7,6 +7,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -15,6 +18,8 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.Glow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
@@ -22,15 +27,18 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Game extends Application {
 
+    private StackPane root;
     private Image bgImage;
     private Label questionLabel;
-    private TextField answerLabel;
+    private TextField answerField;
     private Label statusLabel;
     private ImageView hangman;
     private Image[] pic = new Image[4];
@@ -42,6 +50,7 @@ public class Game extends Application {
     private int question = 0;
     private Label correctLabel;
     private Label wrongLabel;
+    private boolean gameOver = false;
 
     public static void main(String[] args) {
         launch(args);
@@ -53,7 +62,7 @@ public class Game extends Application {
         loadPictures();
         wrong = 0;
         primaryStage.setTitle("Hangman");
-        StackPane root = new StackPane();
+        root = new StackPane();
         addBackground(root);
         addHangman(root);
         BorderPane bp = new BorderPane();
@@ -102,14 +111,14 @@ public class Game extends Application {
     private Node createQA() {
         questionLabel = new Label(questions.get(0));
         questionLabel.setFont(Font.font("serif", FontWeight.BOLD, 20));
-        answerLabel = new TextField();
-        answerLabel.setOnAction((ActionEvent e) -> {
+        answerField = new TextField();
+        answerField.setOnAction((ActionEvent e) -> {
             this.processAnswer();
         });
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(8);
-        vbox.getChildren().addAll(questionLabel, answerLabel);
+        vbox.getChildren().addAll(questionLabel, answerField);
         return (vbox);
     }
 
@@ -139,19 +148,47 @@ public class Game extends Application {
     }
 
     private void processAnswer() {
-        if (answerLabel.getText().equalsIgnoreCase(answers.get(question))) {
+        if (answerField.getText().equalsIgnoreCase(answers.get(question))) {
             statusLabel.setText("Correct!");
             correct++;
             correctLabel.setText("Correct: " + correct);
+            if (question == questions.size() - 1) {
+                gameOver = true;
+                gameWon();
+            }
         } else {
             statusLabel.setText("Your answer was wrong!");
             wrong++;
             wrongLabel.setText("Wrong: " + wrong);
             hangman.setImage(pic[wrong]);
+            if (wrong == pic.length - 1) {
+                gameOver = true;
+                gameLost();
+            }
         }
-        answerLabel.setText("");
-        question++;
-        questionLabel.setText(questions.get(question));
+        answerField.setText("");
+        if (gameOver) {
+            answerField.setDisable(true);
+        } else {
+            question++;
+            questionLabel.setText(questions.get(question));
+        }
+    }
+
+    private void gameLost() {
+        root.getChildren().add(createFinish("You lost!!!"));
+    }
+
+    private static Label createFinish(String text) {
+        Label label = new Label(text);
+        label.setFont(Font.font("serif", FontWeight.BOLD, 44));
+        label.setTextFill(Color.RED);
+        label.setBlendMode(BlendMode.COLOR_BURN);
+        return label;
+    }
+
+    private void gameWon() {
+        root.getChildren().add(createFinish("You won!!!"));
     }
 
 }
